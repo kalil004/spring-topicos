c715cc14231b6a7acfe4181e158e5feb1d6202d2
Kalil Pereira <kalil.pereira@fatec.sp.gov.br>
2025-09-12 13:59:02 -0300

    commit inicial

---
+++ src/test/java/com/kalil/alves/springtopicos/SpringtopicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalil.alves.springtopicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtopicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
